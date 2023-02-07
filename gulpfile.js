36747601beabf8b4b4c1d5845ea736cff23e042a
Rebecca <becca.haf@gmail.com>
2023-02-07 13:56:50 +0000

    github deploy script added

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ var sass = require('gulp-sass')
 var cleanCSS = require('gulp-clean-css')
 var sourcemaps = require('gulp-sourcemaps')
 
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create()
 
 var imagemin = require('gulp-imagemin')
+
+var ghpages = require('gh-pages')
 
 sass.compiler = require('node-sass')
 
@@ -51,6 +53,10 @@ gulp.task("watch", function() {
     gulp.watch("src/css/app.scss", ["sass"])
     gulp.watch("src/fonts/*", ["fonts"])
     gulp.watch("src/img/*", ["images"])
+})
+
+gulp.task("deploy", function() {
+    ghpages.publish("dist")
 })
 
 gulp.task('default', ["html", "sass", "fonts", "images", "watch"])
